7e9cfd7058ba86536aad1ecfbc9c40ad6737feea
Alinur08 <alinurzamanli08@gmail.com>
2026-06-21 02:01:40 +0400

    change

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import numpy as np
 
-
 sensitivity = __import__('1-sensitivity').sensitivity
 precision = __import__('2-precision').precision
+
 
 def f1_score(confusion):
     recall = sensitivity(confusion)
